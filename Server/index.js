e99576039b9a373f93b0c276df7d319c374644be
Sujan2332 <saisujan.s03@gmail.com>
2025-01-02 02:00:22 +0530

    chnage the mongodb route

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cors())
 
 mongoose
-.connect(process.env.MONGO_URI, {useNewUrlParser:true,useUnifiedTopology:true})
+.connect(process.env.MONGO_URI/ChatApp, {useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>console.log('MongoDB Connected Successfully'))
 .catch((err)=>{
     console.error("MongoDB Connection Failed:",err.message)
